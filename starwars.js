d8d03010bc9d5775de2bbd821a8cb8c7236ab998
syahinsyah <syahinsyah7@gmail.com>
2021-02-14 15:33:53 +0800

    array method practice

--- starwars.js
+++ starwars.js
@@ -30,16 +30,76 @@ const characters = [
 ];
 
 
-//Fiter
+// //Filter
 
-const greater100characters = characters.filter( (character) => character.mass > 100 );
-// console.log(greater100characters);
+// const greater100characters = characters.filter( (character) => character.mass > 100 );
+// // console.log(greater100characters);
 
-const shorterCharacters = characters.filter( (character) => character.height <200 );
-// console.log(shorterCharacters);
+// const shorterCharacters = characters.filter( (character) => character.height <200 );
+// // console.log(shorterCharacters);
 
-const maleCharacters = characters.filter( (character) => character.gender ==='male');
-console.log(maleCharacters);
+// const maleCharacters = characters.filter( (character) => character.gender ==='male');
+// // console.log(maleCharacters);
 
-const femaleCharacters = characters.filter( (character) => character.gender === 'female');
-console.log(femaleCharacters);
+// const femaleCharacters = characters.filter( (character) => character.gender === 'female');
+// // console.log(femaleCharacters);
+
+
+// //Map
+
+// const allName =characters.map( (charater) => charater.name);
+// // console.log(allName);
+
+
+// const allHeigt =characters.map( (charater) => charater.height);
+// // console.log(allHeigt);
+
+
+
+// const objectNameandHeight =characters.map( (charater) => ({
+//     name: charater.name,
+//     height: charater.height 
+// }));
+// // console.log(objectNameandHeight); 
+
+
+// const firstName = characters.map( (character) =>
+//     character.name.split(" ")[0]
+// );
+
+// // console.log(firstName);
+
+// //Some
+
+// const oneMaleCharacter = characters.some( (character ) => character.gender ==='male');
+// console.log(oneMaleCharacter);
+
+// const oneBlueCharacter = characters.some( (character) => character.eye_color ==='blue');
+// console.log(oneBlueCharacter);
+
+// const oneTallerthan210 = characters.some( (character) => character.height>210);
+// console.log(oneTallerthan210);
+
+//Sort
+
+// const sortByName = characters.sort( (a,b) =>
+// {
+//     if(a.name < b.name)
+//     {
+//         return -1;
+//     }
+//     return 1;
+// });
+// console.log(sortByName);
+
+// const sortByHeight = characters.sort( (a,b) => a.height-b.height );
+// console.log(sortByHeight);
+
+// const sortbyFemale = characters.sort( (a,b) => 
+// {
+//     if(a.gender ==="female") return -1;
+//     return 1;
+// });
+
+// console.log(sortByHeight);
+
