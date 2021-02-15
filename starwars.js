ccbf0f14d93e5ea27a68b8577712f88e3ca18dfb
syahinsyah <syahinsyah7@gmail.com>
2021-02-15 16:12:11 +0800

    practice array method

--- starwars.js
+++ starwars.js
@@ -1,29 +1,29 @@
 const characters = [
     {
         name: 'Luke Skywalker',
-        height: '172',
-        mass: '77',
+        height: 172,
+        mass: 77,
         eye_color: 'blue',
         gender: 'male',
     },
     {
         name: 'Darth Vader',
-        height: '202',
-        mass: '136',
+        height: 202,
+        mass: 136,
         eye_color: 'yellow',
         gender: 'male',
     },
     {
         name: 'Leia Organa',
-        height: '150',
-        mass: '49',
+        height: 150,
+        mass: 49,
         eye_color: 'brown',
         gender: 'female',
     },
     {
         name: 'Anakin Skywalker',
-        height: '188',
-        mass: '84',
+        height: 188,
+        mass: 84,
         eye_color: 'blue',
         gender: 'male',
     },
@@ -103,3 +103,53 @@ const characters = [
 
 // console.log(sortByHeight);
 
+
+//Reduce 
+
+//soalan 
+// get total mass of all characters. 
+// const sumAllMass = characters.reduce((acc, cur) => {
+//    return acc + cur.mass;
+// },0); // reduce(a , b)
+
+// console.log(sumAllMass);
+
+// const charactersByEyeColor = characters.reduce( (acc, cur) => 
+// {
+//     // console.log(acc[cur.eye_color]);
+//     if(acc[cur.eye_color]){
+//         acc[cur.eye_color] = acc[cur.eye_color] +1 
+//     }else
+//     {
+//         acc[cur.eye_color] =1;
+//     }
+//     return acc;
+// } , {}); //empty object
+
+// console.log(charactersByEyeColor);
+
+
+// const totalName = characters.reduce( (acc , cur ) => {
+//     if(acc[cur.name])
+//     {
+//         acc[cur.name]++;
+//     }
+//     else
+//     {
+//         acc[cur.name]=1;
+//     }
+//     return acc;
+// },{});
+
+// console.log(totalName);
+
+
+//EVERY
+const allBlueEyes = characters.every( (charater ) => {
+    return characters.eye_color === 'blue';
+});
+
+console.log(allBlueEyes);
+
+const allmassmorethan40 = characters.every( (character) => character.mass >40 );
+console.log(allmassmorethan40);
