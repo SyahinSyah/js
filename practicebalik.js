e01e5558145f2e6c65248c7016a387b670a19f75
syahin syah <syahinsyah7@gmail.com>
2021-03-22 22:01:44 +0800

    cuba

---
+++ practicebalik.js
@@ -0,0 +1,21 @@
+const getuser = (id , callback) => {
+    const time = id === 1 ? 3000 : 2000; 
+    setTimeout( () => {
+        const nama = id === 1 ? 'Sandika' : 'Galih';
+        callback({id,nama});
+    },time); 
+};
+
+const userSatu = getUser(1, (hasil) => 
+{
+    console.log(hasil);
+});
+
+const userDua = getUser(2, (hasil) => 
+{
+    console.log(hasil);
+});
+
+
+const halo = 'hellow world!';
+console.log('selesa');
